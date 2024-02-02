7a6a3894dde28ff636521a1b57dbbb3516c5c413
Dylan Zemlin <dylan.zemlin@ou.edu>
2024-02-02 11:33:39 -0600

    Added helper for action values

--- src/main.py
+++ src/main.py
@@ -5,6 +5,13 @@ MAP_NAME = "8x8"
 IS_SLIPPERY = False
 RENDER_MODE = "human"
 
+# Helper ENUM for action space
+class Action:
+    LEFT = 0
+    DOWN = 1
+    RIGHT = 2
+    UP = 3
+
 # Create the environment
 env = gym.make("FrozenLake-v1", desc=None, map_name = MAP_NAME, is_slippery = IS_SLIPPERY, render_mode = RENDER_MODE)
 
