389ca50c49265f337e0141b7b80babb1a6e60b63
Dylan Zemlin <dylan.zemlin@gmail.com>
2024-03-13 10:32:38 -0500

    More montecarlo implementation

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -3,117 +3,65 @@ import numpy as np
 import time
 
 # Set the environment parameters
-MAP_NAME = "8x8"
+MAP_NAME = "4x4"
 IS_SLIPPERY = True
-RENDER_MODE = "none"
-
-# Helper ENUM for action space
-class Action:
-    LEFT = 0
-    DOWN = 1
-    RIGHT = 2
-    UP = 3
 
 # Create the environment
-env = gym.make("FrozenLake-v1", desc=None, map_name = MAP_NAME, is_slippery = IS_SLIPPERY, render_mode = RENDER_MODE)
+env = gym.make("FrozenLake-v1", desc=None, map_name = MAP_NAME, is_slippery = IS_SLIPPERY, max_episode_steps = 100)
 
 # Reset the environment
 env.reset()
 
-# Setup the Q table and hyperparameters
+# Setup the Q tables for the environment and hyperparameters
 Q = np.zeros([env.observation_space.n, env.action_space.n])
-LEARNING_RATE = 0.1
-DISCOUNT_FACTOR = 0.99
-EPISODES = 50000
+Q2 = np.zeros([env.observation_space.n, env.action_space.n])
+rList = []
+LEARNING_RATE = 1
+EPISODES = 500000
 EXPLORATION_RATE = 1
-EXPLORATION_DECAY = 0.005
+EXPLORATION_DECAY = 0.001
 MAX_EXPLORATION_RATE = 1
-MIN_EXPLORATION_RATE = 0.01
-
-# outcomes = []
-# time_per_episode = []
-# last_episode_start = 0
-
-def format_as_minutes_and_seconds(seconds):
-    minutes = int(seconds / 60)
-    seconds = int(seconds % 60)
-    return f"{minutes}m {seconds}s"
-
-def save_q_state():
-    np.save("q_table.npy", Q)
-
-def load_q_state():
-    return np.load("q_table.npy")
+MIN_EXPLORATION_RATE = 0.0001
 
 # Start training
 for i in range(EPISODES):
-    # Print the current episode
-    if i % 10000 == 0:
-        print(f"\rEpisode {i+1}/{EPISODES}", end="")
-    # if len(time_per_episode) > 0:
-        # print(f" - ETA: {format_as_minutes_and_seconds(np.mean(time_per_episode) * (EPISODES - i))}")
-
-    # last_episode_start = time.time()
     observation, rInfo = env.reset()
-    # outcomes.append("Fail")
+    x = 0
+    y = []
+    score = 0
+
     while True:
-        # While we are training, we want to explore the environment so we introduce some randomness into the actions
-        # even if the Q table specifies a "better" action
         rng = np.random.rand()
-        if rng < EXPLORATION_RATE or np.sum(Q[observation]) == 0:
-            action = env.action_space.sample()
-        else:
+        if rng > EXPLORATION_RATE:
             action = np.argmax(Q[observation])
+        else:
+            action = env.action_space.sample()
 
-        # Take the action and observe the outcome
-        nstate, reward, terminated, truncated, info = env.step(action)
-
-        # Calculate the new Q value based on the observed outcome
-        Q[observation, action] = Q[observation, action] + LEARNING_RATE * (reward + DISCOUNT_FACTOR * np.max(Q[nstate]) - Q[observation, action])
-
-        # Update the observation
-        observation = nstate
+        new_state, reward, terminated, truncated, info = env.step(action)
+        x += reward
+        score += reward
+        y.append((observation, action))
+        observation = new_state
 
         # If the episode is terminated, we break the loop
         if terminated:
-            # outcomes[-1] = "Success" if reward > 0 else "Fail"
             break
 
         if truncated:
             break
+
+    rList.append(x)
+
+    for (state, action) in y:
+        Q2[state, action] += 1
+        LEARNING_RATE = 1 / Q2[state, action]
+        Q[state, action] = Q[state, action] + LEARNING_RATE * (score - Q[state, action])
+
+    if i % 10000 == 0 and i != 0:
+        print(str(i) + "/" +str(EPISODES))
+        print("Current score: " + str(sum(rList) / i))
 
     EXPLORATION_RATE = MIN_EXPLORATION_RATE + (MAX_EXPLORATION_RATE - MIN_EXPLORATION_RATE) * np.exp(-EXPLORATION_DECAY * i)
 
-    # time_per_episode.append(time.time() - last_episode_start)
-
-episodes = 1000
-nb_success = 0
-
-# Evaluation
-for _ in range(episodes):
-    observation, rInfo = env.reset()
-    
-    # Until the agent gets stuck or reaches the goal, keep training it
-    while True:
-        # Choose the action with the highest value in the current state
-        action = np.argmax(Q[observation])
-
-        # Implement this action and move the agent in the desired direction
-        new_state, reward, terminated, truncated, info = env.step(action)
-
-        # Update our current state
-        observation = new_state
-
-        # When we get a reward, it means we solved the game
-        nb_success += reward
-
-        if terminated:
-            break
-
-        if truncated:
-            break
-
 # Let's check our success rate!
-print (f"Success rate = {nb_success/episodes*100}%")
-
-save_q_state()
+print (f"Success rate = {sum(rList)/EPISODES}")
