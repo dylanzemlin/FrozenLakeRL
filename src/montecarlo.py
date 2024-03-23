6c3a7cfe0f82dfadb9b9b830893146038c9c42d2
Dylan Zemlin <dylan.zemlin@gmail.com>
2024-03-22 20:47:47 -0600

    Push some montecarlo goodness (its kinda bad)

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -1,13 +1,16 @@
+# Dylan Zemlin | 113562763
+# Machine Learning (CS 5033)
+# Reinforcement Learning Project | Monte Carlo Algorithm
+
 import gymnasium as gym
 import numpy as np
-import time
 
 # Set the environment parameters
-MAP_NAME = "4x4"
+MAP_NAME = "8x8"
 IS_SLIPPERY = True
 
 # Create the environment
-env = gym.make("FrozenLake-v1", desc=None, map_name = MAP_NAME, is_slippery = IS_SLIPPERY, max_episode_steps = 100)
+env = gym.make("FrozenLake-v1", desc=None, map_name = MAP_NAME, is_slippery = IS_SLIPPERY)
 
 # Reset the environment
 env.reset()
@@ -17,7 +20,7 @@ Q = np.zeros([env.observation_space.n, env.action_space.n])
 Q2 = np.zeros([env.observation_space.n, env.action_space.n])
 rList = []
 LEARNING_RATE = 1
-EPISODES = 500000
+EPISODES = 25000
 EXPLORATION_RATE = 1
 EXPLORATION_DECAY = 0.001
 MAX_EXPLORATION_RATE = 1
@@ -25,42 +28,47 @@ MIN_EXPLORATION_RATE = 0.0001
 
 # Start training
 for i in range(EPISODES):
+    # Reset the environment
     observation, rInfo = env.reset()
+
+    # Initialize the variables
     x = 0
     y = []
-    score = 0
 
     while True:
+        # Choose an action by picking from the Q table or randomly
         rng = np.random.rand()
         if rng > EXPLORATION_RATE:
             action = np.argmax(Q[observation])
         else:
             action = env.action_space.sample()
 
+        # Take the action and observe the outcome
         new_state, reward, terminated, truncated, info = env.step(action)
+
+        # Update the Q table with the new information and reward
         x += reward
-        score += reward
         y.append((observation, action))
         observation = new_state
 
         # If the episode is terminated, we break the loop
-        if terminated:
+        if terminated or truncated:
             break
 
-        if truncated:
-            break
-
+    # Update the reward list
     rList.append(x)
 
+    # Update the Q table with the new information and reward
     for (state, action) in y:
         Q2[state, action] += 1
         LEARNING_RATE = 1 / Q2[state, action]
-        Q[state, action] = Q[state, action] + LEARNING_RATE * (score - Q[state, action])
+        Q[state, action] = Q[state, action] + LEARNING_RATE * (x - Q[state, action])
 
+    # Print the progress
     if i % 10000 == 0 and i != 0:
         print(str(i) + "/" +str(EPISODES))
-        print("Current score: " + str(sum(rList) / i))
 
+    # Update the exploration rate via a exponential decay function
     EXPLORATION_RATE = MIN_EXPLORATION_RATE + (MAX_EXPLORATION_RATE - MIN_EXPLORATION_RATE) * np.exp(-EXPLORATION_DECAY * i)
 
 # Let's check our success rate!
